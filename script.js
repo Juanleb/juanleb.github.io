7233f06d4a44b06cc48b8ecddec399656a472ed7
Juanleb <85050430+Juanleb@users.noreply.github.com>
2024-12-23 20:17:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Configurar ScrollReveal solo para el texto
+ScrollReveal().reveal('.animated-text-star', {
+    origin: 'bottom', // Origen de la animación
+    distance: '50px', // Distancia desde el origen
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de interpolación
+    interval: 200 // Intervalo entre animaciones
+});
+
+ScrollReveal().reveal('.animated-text', {
+    origin: 'left', // Origen de la animación
+    distance: '50px', // Distancia desde el origen
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de interpolación
+    interval: 200 // Intervalo entre animaciones
+});
+
+// Configurar ScrollReveal solo para el texto
+ScrollReveal().reveal('.animated-text-about-us', {
+    origin: 'right', // Origen de la animación
+    distance: '50px', // Distancia desde el origen
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de interpolación
+    interval: 200 // Intervalo entre animaciones
+});
+
+// Configurar ScrollReveal solo para el texto
+ScrollReveal().reveal('.animated-text-partners', {
+    origin: 'right', // Origen de la animación
+    distance: '50px', // Distancia desde el origen
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de interpolación
+    interval: 200 // Intervalo entre animaciones
+});
+
+// Configurar ScrollReveal solo para el texto
+ScrollReveal().reveal('.animated-text-tokenomic', {
+    origin: 'top', // Origen de la animación
+    distance: '50px', // Distancia desde el origen
+    duration: 1000, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Tipo de interpolación
+    interval: 200 // Intervalo entre animaciones
+});
